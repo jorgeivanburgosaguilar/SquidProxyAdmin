d85934c461ecca442c07109b4289908363475d5e
Jorge Ivan Burgos Aguilar <15068542+jorgeivanburgosaguilar@users.noreply.github.com>
2023-05-04 10:54:19 -0600

    Avances de pasar de python2 a python3 la parte del sistema que es de cli y usar mensajes genericos.

--- FiltroSquid-CLI/Funciones.py
+++ FiltroSquid-CLI/Funciones.py
@@ -1,116 +1,119 @@
-#!/usr/bin/python -u
-# -*- coding: utf-8 -*-
-# Funciones comunes de los Filtros
+#!/usr/bin/env python
+''' funciones comunes '''
 import os
 import sys
 import socket
-from urlparse import urlparse
+from urllib.parse import urlparse
 
 
 def obtener_ruta_configuraciones():
-    return os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Configuraciones'))
+  current_directory = os.path.dirname(os.path.realpath(__file__))
+  return os.path.abspath(os.path.join(current_directory, 'Configuraciones'))
+
+
+def obtener_ruta_tlds():
+  return os.path.join(obtener_ruta_configuraciones(), 'effective_tld_names.dat')
 
 
 # TLDs Globales Validos
-# Obtenidos de https://publicsuffix.org/list/effective_tld_names.dat
-with open(os.path.join(obtener_ruta_configuraciones(), 'effective_tld_names.dat'), "r") as archivo:
-    tlds = [linea.strip() for linea in archivo if linea[0] not in "/\n"]
+with open(obtener_ruta_tlds(), 'r', encoding='utf-8') as archivo:
+  tlds = [linea.strip() for linea in archivo if linea[0] not in '/\n']
 
 
 def squid_mensaje(tipo='OK', mensaje='', registro=''):
-    '''Imprime una mensaje de estado con el formato para los ACL Externos de Squid 3.x '''
-    if mensaje:
-        mensaje = ' message={0}'.format(mensaje)
+  '''Imprime una mensaje con el formato para los ACL Externos de Squid 3.x '''
+  if mensaje:
+    mensaje = f' message={mensaje}'
 
-    if registro:
-        registro = ' log={0}'.format(registro)
+  if registro:
+    registro = f' log={registro}'
 
-    sys.stdout.write('{0}{1}{2}\n'.format(tipo.upper(), mensaje, registro))
+  sys.stdout.write(f'{tipo.upper()}{mensaje}{registro}\n')
 
 
 def es_una_ipv4_valida(ip):
-    '''Funcion para validar una IPv4'''
-    try:
-        socket.inet_aton(ip)
-        return True
-    except socket.error:
-        return None
-
-
-def es_un_dominio_valido(dominio):
-    '''Funcion para verificar que el dominio sea valido'''
-    dominio = 'http://{0}/'.format(dominio)
-
-    # Obten el Host de la URI
-    urlElements = urlparse(dominio)[1]
-
-    # Si el host es una direccion IPv4 entonces es un dominio valido
-    if (es_una_ipv4_valida(urlElements)):
-        return True
-
-    urlElements = urlElements.lower().split('.')
-    for i in range(-len(urlElements),0):
-        lastIElements = urlElements[i:]
-        
-        candidate = ".".join(lastIElements)
-        wildcardCandidate = ".".join(["*"]+lastIElements[1:])
-        exceptionCandidate = "!" + candidate
-
-        if (exceptionCandidate in tlds):
-            return True
-
-        if (candidate in tlds or wildcardCandidate in tlds):
-            return True
-        
+  '''Funcion para validar una IPv4'''
+  try:
+    socket.inet_aton(ip)
+    return True
+  except socket.error:
     return None
 
 
+def es_un_dominio_valido(dominio):
+  '''Funcion para verificar que el dominio sea valido'''
+  dominio = f'http://{dominio}/'
+
+  # Obten el Host de la URI
+  urlElements = urlparse(dominio)[1]
+
+  # Si el host es una direccion IPv4 entonces es un dominio valido
+  if (es_una_ipv4_valida(urlElements)):
+    return True
+
+  urlElements = urlElements.lower().split('.')
+  for i in range(-len(urlElements), 0):
+    lastIElements = urlElements[i:]
+
+    candidate = '.'.join(lastIElements)
+    wildcardCandidate = '.'.join(['*'] + lastIElements[1:])
+    exceptionCandidate = '!' + candidate
+
+    if (exceptionCandidate in tlds):
+      return True
+
+    if (candidate in tlds or wildcardCandidate in tlds):
+      return True
+
+  return None
+
+
 def obtener_dominio(dominio, efectivo=False):
-    '''Funcion para obtener el host/dominio de una URL'''
-    dominio = 'http://{0}/'.format(dominio)
+  '''Funcion para obtener el host/dominio de una URL'''
+  dominio = 'http://{0}/'.format(dominio)
 
-    # Obten el Host de la URI
-    urlElements = urlparse(dominio)[1]
+  # Obten el Host de la URI
+  urlElements = urlparse(dominio)[1]
 
-    # Si no estamos buscando el host efectivo devuelvelo asi como esta
-    if (not efectivo):
-        return urlElements.lower()
+  # Si no estamos buscando el host efectivo devuelvelo asi como esta
+  if not efectivo:
+    return urlElements.lower()
 
-    # Si el host es una direccion IPv4 entonces deja de procesar y devuelve la
-    # direccion como host
-    if (es_una_ipv4_valida(urlElements)):
-        return urlElements
+  # Si el host es una direccion IPv4 entonces deja de procesar y devuelve la
+  # direccion como host
+  if (es_una_ipv4_valida(urlElements)):
+    return urlElements
 
-    urlElements = urlElements.lower().split('.')
-    for i in range(-len(urlElements),0):
-        lastIElements = urlElements[i:]
+  urlElements = urlElements.lower().split('.')
+  for i in range(-len(urlElements), 0):
+    lastIElements = urlElements[i:]
 
-        candidate = ".".join(lastIElements)
-        wildcardCandidate = ".".join(["*"]+lastIElements[1:])
-        exceptionCandidate = "!" + candidate
+    candidate = '.'.join(lastIElements)
+    wildcardCandidate = '.'.join(['*'] + lastIElements[1:])
+    exceptionCandidate = '!' + candidate
 
-        if (exceptionCandidate in tlds):
-            return ".".join(urlElements[i:])
+    if exceptionCandidate in tlds:
+      return '.'.join(urlElements[i:])
 
-        if (candidate in tlds or wildcardCandidate in tlds):
-            return ".".join(urlElements[i-1:])
-    
-    raise ValueError("Dominio Invalido")
+    if (candidate in tlds or wildcardCandidate in tlds):
+      return '.'.join(urlElements[i - 1:])
+
+  raise ValueError('Dominio Invalido')
 
 
 def depurar_dominios(texto):
-    '''
-    Funcion para ordenar y eliminar dominios invalidos asi como duplicados
-    de un cadena separada por retornos de linea.
-    Devuelve una lista/coleccion
-    '''
-    registros_unicos = []
-    lista = texto.split("\n")
+  '''
+  Funcion para ordenar y eliminar dominios invalidos asi como duplicados
+  de un cadena separada por retornos de linea.
+  Devuelve una lista/coleccion
+  '''
+  registros_unicos = []
+  lista = texto.split('\n')
 
-    for registro in lista:
-        registro = registro.rstrip(". '\"\r\t").lstrip().lower()
+  for registro in lista:
+    registro = registro.rstrip('. "\'\r\t').lstrip().lower()
 
-        if (len(registro) > 2 and es_un_dominio_valido(registro)):
-            registros_unicos.append(registro)
+    if (len(registro) > 2 and es_un_dominio_valido(registro)):
+      registros_unicos.append(registro)
 
-    return registros_unicos
+  return registros_unicos
--- FiltroSquid-CLI/ObtenerAsignacionesParaDHCP.py
+++ FiltroSquid-CLI/ObtenerAsignacionesParaDHCP.py
@@ -1,34 +1,34 @@
-#!/usr/bin/python -u
-# -*- coding: utf-8 -*-
-# Obtener Asignaciones para DHCP
-# Genera las asignaciones configuradas en la base de datos para DHCP (Dnsmasq)
+#!/usr/bin/env python
+'''
+Obtener Asignaciones para DHCP
+Genera las asignaciones configuradas en la base de datos para DHCP (Dnsmasq)
+'''
+import django
 import os
 import sys
-from ConfigParser import SafeConfigParser
-from Funciones import obtener_ruta_configuraciones
+from configparser import ConfigParser
+from funciones import obtener_ruta_configuraciones
 
 
 # Cargar configuraciones
-configParser = SafeConfigParser()
-configParser.read(os.path.join(obtener_ruta_configuraciones(), 'principal.conf'))
+configParser = ConfigParser()
+configParser.read(os.path.join(
+  obtener_ruta_configuraciones(), 'principal.conf'))
 
 # Directivas del Filtro
 en_produccion = configParser.getboolean('FiltroWeb', 'Produccion')
 
 # Cargar backend basado en Django
-import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SquidProxyAdmin.settings')
 django.setup()
 
-# Importes de Django
 from Administrador.models import Asignacion
 
 try:
-    asignaciones = Asignacion.objects.all().values('ip', 'mac')
-    for asignacion in asignaciones:
-        sys.stdout.write('dhcp-host={0},192.168.2.{1}\n'.format(asignacion['mac'].upper(), asignacion['ip']))
-
-
+  asignaciones = Asignacion.objects.all().values('ip', 'mac')
+  for asignacion in asignaciones:
+    sys.stdout.write(
+      f'dhcp-host={asignacion["mac"].upper()},192.168.2.{asignacion["ip"]}\n')
 except Exception as e:
-    if not en_produccion:
-        sys.stdout.write('{0}: {1}'.format(str(type(e)), str(e)))
+  if not en_produccion:
+    sys.stdout.write(f'{str(type(e))}: {str(e)}')
