49af66890e09f2fd03f65f5ae358e2843bf40061
Jorge Ivan Burgos Aguilar <15068542+jorgeivanburgosaguilar@users.noreply.github.com>
2023-05-09 16:04:56 -0600

    fin de corregir problemas de linter en funciones comunes asi como las adecuaciones y soluciones para el script que obtiene las asignaciones para el servidor de dhcp (dnsmasq), queda pendiente refactorizar codigo.

--- FiltroSquid-CLI/Funciones.py
+++ FiltroSquid-CLI/Funciones.py
@@ -45,24 +45,24 @@ def es_un_dominio_valido(dominio):
   dominio = f'http://{dominio}/'
 
   # Obten el Host de la URI
-  urlElements = urlparse(dominio)[1]
+  url_elements = urlparse(dominio)[1]
 
   # Si el host es una direccion IPv4 entonces es un dominio valido
-  if (es_una_ipv4_valida(urlElements)):
+  if es_una_ipv4_valida(url_elements):
     return True
 
-  urlElements = urlElements.lower().split('.')
-  for i in range(-len(urlElements), 0):
-    lastIElements = urlElements[i:]
+  url_elements = url_elements.lower().split('.')
+  for i in range(-len(url_elements), 0):
+    last_ielements = url_elements[i:]
 
-    candidate = '.'.join(lastIElements)
-    wildcardCandidate = '.'.join(['*'] + lastIElements[1:])
-    exceptionCandidate = '!' + candidate
+    candidate = '.'.join(last_ielements)
+    wildcard_candidate = '.'.join(['*'] + last_ielements[1:])
+    exception_candidate = '!' + candidate
 
-    if (exceptionCandidate in tlds):
+    if exception_candidate in tlds:
       return True
 
-    if (candidate in tlds or wildcardCandidate in tlds):
+    if (candidate in tlds or wildcard_candidate in tlds):
       return True
 
   return None
@@ -70,41 +70,42 @@ def es_un_dominio_valido(dominio):
 
 def obtener_dominio(dominio, efectivo=False):
   '''Funcion para obtener el host/dominio de una URL'''
-  dominio = 'http://{0}/'.format(dominio)
+  dominio = f'http://{dominio}/'
 
   # Obten el Host de la URI
-  urlElements = urlparse(dominio)[1]
+  url_elements = urlparse(dominio)[1]
 
   # Si no estamos buscando el host efectivo devuelvelo asi como esta
   if not efectivo:
-    return urlElements.lower()
+    return url_elements.lower()
 
-  # Si el host es una direccion IPv4 entonces deja de procesar y devuelve la
-  # direccion como host
-  if (es_una_ipv4_valida(urlElements)):
-    return urlElements
+  # Si el host es una direccion IPv4 entonces deja de
+  # procesar y devuelve la direccion como host
+  if es_una_ipv4_valida(url_elements):
+    return url_elements
 
-  urlElements = urlElements.lower().split('.')
-  for i in range(-len(urlElements), 0):
-    lastIElements = urlElements[i:]
+  url_elements = url_elements.lower().split('.')
+  for i in range(-len(url_elements), 0):
+    last_ielements = url_elements[i:]
 
-    candidate = '.'.join(lastIElements)
-    wildcardCandidate = '.'.join(['*'] + lastIElements[1:])
-    exceptionCandidate = '!' + candidate
+    candidate = '.'.join(last_ielements)
+    wildcard_candidate = '.'.join(['*'] + last_ielements[1:])
+    exception_candidate = '!' + candidate
 
-    if exceptionCandidate in tlds:
-      return '.'.join(urlElements[i:])
+    if exception_candidate in tlds:
+      return '.'.join(url_elements[i:])
 
-    if (candidate in tlds or wildcardCandidate in tlds):
-      return '.'.join(urlElements[i - 1:])
+    if (candidate in tlds or wildcard_candidate in tlds):
+      return '.'.join(url_elements[i - 1:])
 
   raise ValueError('Dominio Invalido')
 
 
 def depurar_dominios(texto):
   '''
-  Funcion para ordenar y eliminar dominios invalidos asi como duplicados
-  de un cadena separada por retornos de linea.
+  Funcion para ordenar y eliminar dominios invalidos
+  asi como duplicados de un cadena separada
+  por retornos de linea.
   Devuelve una lista/coleccion
   '''
   registros_unicos = []
--- FiltroSquid-CLI/ObtenerAsignacionesParaDHCP.py
+++ FiltroSquid-CLI/ObtenerAsignacionesParaDHCP.py
@@ -3,27 +3,31 @@
 Obtener Asignaciones para DHCP
 Genera las asignaciones configuradas en la base de datos para DHCP (Dnsmasq)
 '''
-import django
 import os
 import sys
+import django
 from configparser import ConfigParser
 from funciones import obtener_ruta_configuraciones
 
-
 # Cargar configuraciones
-configParser = ConfigParser()
-configParser.read(os.path.join(
+config_parser = ConfigParser()
+config_parser.read(os.path.join(
   obtener_ruta_configuraciones(), 'principal.conf'))
 
 # Directivas del Filtro
-en_produccion = configParser.getboolean('FiltroWeb', 'Produccion')
+en_produccion = config_parser.getboolean('FiltroWeb', 'Produccion')
+current_path = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(current_path)
 
 # Cargar backend basado en Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SquidProxyAdmin.settings')
 django.setup()
 
+# pylint: disable=wrong-import-position
 from Administrador.models import Asignacion
+# pylint: enable=wrong-import-position
 
+# pylint: disable=broad-exception-caught
 try:
   asignaciones = Asignacion.objects.all().values('ip', 'mac')
   for asignacion in asignaciones:
@@ -32,3 +36,5 @@ try:
 except Exception as e:
   if not en_produccion:
     sys.stdout.write(f'{str(type(e))}: {str(e)}')
+
+# pylint: enable=broad-exception-caught
