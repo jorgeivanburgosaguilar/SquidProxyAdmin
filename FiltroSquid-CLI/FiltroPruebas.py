bd724aa4663a4945c2885f9d511402fc6f389380
Jorge Ivan Burgos Aguilar <15068542+jorgeivanburgosaguilar@users.noreply.github.com>
2023-05-16 10:47:10 -0600

    Soporte para python3 para el filtro de pruebas

--- FiltroSquid-CLI/FiltroPruebas.py
+++ FiltroSquid-CLI/FiltroPruebas.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python -u
-# -*- coding: utf-8 -*-
-# Filtrado Web para Probar los diferentes Formatos en Squid
-# ACL Externa para Squid 3.5
+#!/usr/bin/env python
+'''
+Filtrado Web para Probar los diferentes Formatos en Squid
+ACL Externa para Squid 3.5
+'''
 import sys
-from urllib import quote
-
+from urllib.parse import quote
 
 EOF = False
 while not EOF:
-    line = sys.stdin.readline()
-    if not line:
-        EOF = True
-        continue
-    
-    sys.stdout.write('OK message={0} log={0}\n'.format(quote(line)))
+  line = sys.stdin.readline()
+  if not line:
+    EOF = True
+    continue
+
+  sys.stdout.write(f'OK message={line} log={quote(line)}\n')
