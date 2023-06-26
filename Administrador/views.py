78d495ca530a53877a10be0dad2b035cb5dcc5be
Jorge Ivan Burgos Aguilar <15068542+jorgeivanburgosaguilar@users.noreply.github.com>
2023-06-26 12:55:37 -0600

    Corregir API que genera la lista de IP's actuales en caso de que le envien una cadena o un parametro vacio asi como el caso en el que no se tenga reservado ninguna direccion en el DHCP

--- Administrador/views.py
+++ Administrador/views.py
@@ -27,19 +27,27 @@ def update_ip_choices(request):
 def get_updated_ip_choices(red_id):
   ip_choices = [('', '---------')]
   try:
+    if not red_id:
+      raise Red.DoesNotExist
+
     red = Red.objects.get(pk=red_id)
     network = ipaddress.ip_network(red.cidr)
     ips_asignadas = list(Asignacion.objects.values_list('ip', flat=True))
-    ips_rango_dinamico = generar_rango_ip(
-      network, red.inicio_dhcp, red.fin_dhcp)
-    ips_ocupadas = tuple(ips_asignadas + ips_rango_dinamico)
+
+    ips_ocupadas: tuple = None
+    if not red.inicio_dhcp or not red.fin_dhcp:
+      ips_ocupadas = tuple(ips_asignadas)
+    else:
+      ips_rango_dinamico = generar_rango_ip(
+        network, red.inicio_dhcp, red.fin_dhcp)
+      ips_ocupadas = tuple(ips_asignadas + ips_rango_dinamico)
 
     for ip in network.hosts():
       ip_str = str(ip)
       if ip_str not in ips_ocupadas:
         ip_choices.append((ip_str, ip_str))
 
-  except Red.DoesNotExist:
+  except (Red.DoesNotExist, ValueError):
     pass
 
   return {'choices': ip_choices}
